8bead89f20d77d782127ca7912da53ba2cf32d80
Ismael Triana Artola <ismael.triana@ntsprint.com>
2024-03-29 16:13:53 -0400

    Adding Algorithm Challenges

---
+++ VamosTodos_Test/VamosTodos_Test.AlphabeticalOrderChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingrese una frase:");
+string frase = Console.ReadLine();
+
+string resultado = OrdenarLetrasEnPalabras(frase);
+Console.WriteLine("Resultado: " + resultado);
+
+static string OrdenarLetrasEnPalabras(string frase)
+{
+    string[] palabras = frase.Split(' ');
+
+    for (int i = 0; i < palabras.Length; i++)
+    {
+        palabras[i] = OrdenarPalabra(palabras[i]);
+    }
+
+    return string.Join(" ", palabras);
+}
+
+static string OrdenarPalabra(string palabra)
+{
+    char[] letras = palabra.ToCharArray();
+    Array.Sort(letras, (x, y) => char.ToLower(x).CompareTo(char.ToLower(y)));
+    return new string(letras);
+}
---
+++ VamosTodos_Test/VamosTodos_Test.SalaryCalculationChallenge/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Ejemplo de uso de la función
+Console.WriteLine("Ingrese la hora de inicio del trabajador (HH:MM):");
+string horaInicioStr = Console.ReadLine();
+
+Console.WriteLine("Ingrese la hora de fin del trabajador (HH:MM):");
+string horaFinStr = Console.ReadLine();
+
+Console.WriteLine("Ingrese el salario por hora (horario normal):");
+double salarioNormal = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el Factor multiplicador por hora (horario normal):");
+double factorMultiplicador = Convert.ToDouble(Console.ReadLine());
+
+double salarioTotal = CalcularSalarioTotal(horaInicioStr, horaFinStr, salarioNormal, factorMultiplicador);
+Console.WriteLine("El salario total del día es: $" + salarioTotal);
+
+static double CalcularSalarioTotal(string horaInicioStr, string horaFinStr, double salarioNormal, double factorMultiplicador)
+{
+    // Convertir las horas de inicio y fin a objetos DateTime
+    DateTime horaInicio = DateTime.ParseExact(horaInicioStr, "HH:mm", null);
+    DateTime horaFin = DateTime.ParseExact(horaFinStr, "HH:mm", null);
+
+    // Definir el horario normal de trabajo
+    DateTime horaInicioNormal = DateTime.ParseExact("08:00", "HH:mm", null);
+    DateTime horaFinNormal = DateTime.ParseExact("18:00", "HH:mm", null);
+
+    double salarioTotal = 0;
+
+    // Calcular salario durante el horario normal
+    if (horaInicio >= horaInicioNormal && horaFin <= horaFinNormal)
+    {
+        // El trabajador trabajó todo el día en horario normal
+        salarioTotal = (horaFin - horaInicio).TotalHours * salarioNormal;
+    }
+    else
+    {
+        double horasExtras = 0;        
+
+        // Calcular salario fuera del horario normal
+        if (horaInicio < horaInicioNormal)
+            horasExtras += (horaInicioNormal - horaInicio).TotalHours;
+        if (horaFin > horaFinNormal)
+            horasExtras += (horaFin - horaFinNormal).TotalHours;
+
+        // Calcular salario fuera del horario normal
+        double horasNormales = ((horaFin - horaInicio).TotalHours - horasExtras);
+
+        salarioTotal = (horasNormales * salarioNormal) + (horasExtras * salarioNormal * factorMultiplicador);
+    }
+
+    return salarioTotal;
+}
